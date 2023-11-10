0520e53bd723c9c9c357e5063d1419aba010f73f
Adonis Rebano <earebano01@monccnb.ca>
2023-11-10 18:42:07 -0500

    chargé JSON depuis serveur API, intégré avec chart

--- ardjson/ardjson/urls.py
+++ ardjson/ardjson/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import routers
 from . import views
 
-# router = routers.SimpleRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    # path('api/', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls')),
     path('json/', views.jsonView, name='json'),    
     path('humidity/', views.humidityView, name='humidity'),    
 ]
